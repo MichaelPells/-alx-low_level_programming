b56f9f43df000e43e89de4082296af8cf9519985
MichaelPells <themichaelpells@gmail.com>
2022-07-05 09:42:52 -0700

    06

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - This is a description
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
