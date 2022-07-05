cfcfdffe712cea8b86521abbe8dd120037f3b835
MichaelPells <themichaelpells@gmail.com>
2022-07-05 05:31:27 -0700

    02

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - This is a description
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	char n;
+	int i;
+
+	n = 'a';
+
+	i = 0;
+	while (i < 26)
+	{
+		putchar(n);
+		n++;
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
