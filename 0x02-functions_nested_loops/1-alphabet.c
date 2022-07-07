390be1b97f311f8c639537249514bd4a844227f8
MichaelPells <themichaelpells@gmail.com>
2022-07-06 17:57:43 -0700

    01

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - This is a description
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	a = 'a';
+
+	while (a <= 'z')
+		_putchar(a++);
+
+	_putchar('\n');
+}
+
