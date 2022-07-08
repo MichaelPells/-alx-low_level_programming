293891fdbeecf89c88df72c927997a82c859a470
MichaelPells <themichaelpells@gmail.com>
2022-07-07 17:04:35 -0700

    All done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prime factors
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long n;
+	int i;
+	int j;
+	int isPrime;
+
+	n = 612852475143;
+	n = (int) (n / 568);
+
+	while (1)
+	{
+		for (i = 2; i <= n; i++)
+		{
+			for (j = 2; j < i; j++)
+			{
+				if ((i % j) == 0)
+				{
+					isPrime = 0;
+					break;
+				}
+				isPrime = 1;
+			}
+
+			if (isPrime)
+			{
+				if ((n % i) == 0)
+				{
+					n /= i;
+					if (n == 1)
+						printf("%d\n", i);
+
+					break;
+				}
+			}
+		}
+
+		if (n == 1)
+			break;
+	}
+
+	return (0);
+}
+
