9a83b6b95cb3fce21377e4f23fc5fcfbb66a2372
MichaelPells <themichaelpells@gmail.com>
2022-07-10 11:33:56 -0700

    new commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - This is a description
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
