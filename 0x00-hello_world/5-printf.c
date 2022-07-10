cee7fac891084f67d1a7badd3fef31d707e63463
MichaelPells <themichaelpells@gmail.com>
2022-07-10 11:36:06 -0700

    new commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - This is a description
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
