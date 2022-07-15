bbb8859fae827911c5aab25cd7a2cc320e025dea
MichaelPells <themichaelpells@gmail.com>
2022-07-14 17:33:27 -0700

    new commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,25 +7,12 @@
  */
 char *leet(char *str)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < (int) strlen(str); i++)
 	{
 		if (str[i] == 'a' || str[i] == 'A')
 			str[i] = '4';
-		if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
-	}
-	for (j = 0; j < 2; j++)
-	{
-		if (j == 2)
-			j--;
 	}
 	return (str);
 }
