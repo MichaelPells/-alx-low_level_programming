e9c13dd537c88e79d4b56c3642df0b38c09a5829
MichaelPells <themichaelpells@gmail.com>
2022-09-30 13:44:12 +0000

    0x09-static_libraries

---
+++ 8-print_diagsums.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * print_diagsums - 0
+ * @a: a
+ * @size: 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, x, y;
+	float c;
+
+	x = 0;
+	y = 0;
+
+	for (i = 0; i < size * size; i++)
+	{
+		if (i == (size * floor(i / size)) + floor(i / size))
+		{
+			x += a[i];
+		}
+
+		c = (float) (i + 1);
+
+		if (i == size * ceilf(c  / (float) size) - ceilf(c  / (float) size))
+		{
+			y += a[i];
+		}
+	}
+	printf("%d, %d\n", x, y);
+}
+
